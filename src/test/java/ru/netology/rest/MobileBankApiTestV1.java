77fb017fbea0025bfa7ee2fb70b0d4e3d8daf18d
Vladimir <vova,erohind82@mail.ru>
2023-04-12 11:32:49 +0300

    src

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,10 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matcher.*;
 
 class MobileBankApiTestV1 {
     @Test
--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -1,3 +1,4 @@
+
 package ru.netology.rest;
 
 import io.restassured.http.ContentType;
